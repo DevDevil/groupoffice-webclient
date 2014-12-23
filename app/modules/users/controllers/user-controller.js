fe461d9d52a0810f8379df581559422a66294b13
wsmits <wsmits@mailer@intermesh.dev>
2014-12-23 15:55:52 +0100

    Added form

--- app/modules/users/controllers/user-controller.js
+++ app/modules/users/controllers/user-controller.js
@@ -2,20 +2,38 @@
 
 /* Controllers */
 angular.module('GO.users.controllers').
-	controller('UserController', ['$scope', '$state', 'Translate', 'Store', 'Model', 'Tags', 'CustomFields','Modules', function($scope, $state, Translate, Store, Model, Tags, CustomFields, Modules) {
+	controller('UserController', ['$scope', '$state', '$stateParams', 'Translate', 'Store', 'Model', 'Tags', 'CustomFields','Modules', function($scope, $state, $stateParams, Translate, Store, Model, Tags, CustomFields, Modules) {
 
 		$scope.pageTitle = Translate.t('Users');
 
 		/* For mobiles, switch list and details on state */
 
-		$scope.store = new Store('auth/users',{returnAttributes: "*"});
-		$scope.store.load();
+//		console.log($state.params);
+//		console.log($stateParams);
+
+		$scope.userStore = new Store('auth/users',{returnAttributes: "*"});
+
+		if($stateParams.role){
+			//$scope.userStore.searchQuery = $stateParams.search;
+
+			var where = [{
+				"userRole.roleId": $stateParams.role
+			}];
+			$scope.userStore.load({where: where});
+		} else {
+			$scope.userStore.load();
+		}
+
+
+
+		$scope.roleStore = new Store('auth/roles',{returnAttributes: "*"});
+		$scope.roleStore.load();
 
 		//Will be used in child scope. We define it here so we can access
 		//the properties if needed in the future.
 		//Child scopes automatically inherit properties of the parents but
 		//not the other way around.
-		$scope.user = new Model('auth/users',{returnAttributes: "*"});
+		$scope.user = new Model('auth/users',{returnAttributes: "*, roles, userRole"});
 
 		/* End select options for detail and edit controller */
 		$scope.save = function() {
@@ -29,12 +47,12 @@ angular.module('GO.users.controllers').
 		};
 
 		$scope.syncWithStore = function(reloadStore) {
-			var index = $scope.store.findIndexByAttribute('id', $scope.user.id);
+			var index = $scope.userStore.findIndexByAttribute('id', $scope.user.id);
 
 			if (index > -1) {
-				$scope.store.items[index].attributes = angular.copy($scope.user.attributes);
+				$scope.userStore.items[index].attributes = angular.copy($scope.user.attributes);
 			} else if (reloadStore) {
-				$scope.store.reload();
+				$scope.userStore.reload();
 			}
 		};
 
