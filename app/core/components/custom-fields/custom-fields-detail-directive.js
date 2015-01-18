5021a07d738676ebf6cf06dabc8ad535e832b82e
Merijn Schering <mschering@intermesh.nl>
2015-01-18 11:42:30 +0100

    view custom fields

--- app/core/components/custom-fields/custom-fields-detail-directive.js
+++ app/core/components/custom-fields/custom-fields-detail-directive.js
@@ -4,32 +4,32 @@ angular.module('GO.core')
 		.directive('goCustomFieldsDetail', ['$templateCache', function($templateCache) {
 
 				$templateCache.put('customfield-text-detail.html',
-						'<dl ng-if="!$root.isEmpty(imModel.customfields[field.databaseName])">\
+						'<dl ng-if="!$root.isEmpty(goModel[field.databaseName])">\
 							<dt>{{field.name}}</dt>\
-							<dd>{{imModel.customfields[field.databaseName]}}</dd>\
+							<dd>{{goModel[field.databaseName]}}</dd>\
 						</dl>');
 
 				$templateCache.put('customfield-textarea-detail.html',
-						'<dl ng-if="!$root.isEmpty(imModel.customfields[field.databaseName])">\
+						'<dl ng-if="!$root.isEmpty(goModel[field.databaseName])">\
 							<dt>{{field.name}}</dt>\
-							<dd><pre>{{imModel.customfields[field.databaseName]}}</pre></dd>\
+							<dd><pre>{{goModel[field.databaseName]}}</pre></dd>\
 						</dl>');
 				
 				$templateCache.put('customfield-select-detail.html',
-						'<dl ng-if="!$root.isEmpty(imModel.customfields[field.databaseName])">\
+						'<dl ng-if="!$root.isEmpty(goModel[field.databaseName])">\
 							<dt>{{field.name}}</dt>\
-							<dd>{{imModel.customfields[field.databaseName]}}</dd>\
+							<dd>{{goModel[field.databaseName]}}</dd>\
 						</dl>');
 						
 				$templateCache.put('customfield-checkbox-detail.html',
 						'<dl>\
-							<dt>&nbsp;</dt><dd><i class="fa" ng-class="{\'fa-square-o\': !imModel.customfields[field.databaseName],\'fa-check-square-o\': imModel.customfields[field.databaseName]}"></i> {{field.name}}</dd>\
+							<dt>&nbsp;</dt><dd><i class="fa" ng-class="{\'fa-square-o\': !goModel[field.databaseName],\'fa-check-square-o\': goModel[field.databaseName]}"></i> {{field.name}}</dd>\
 						</dl>');
 						
 				$templateCache.put('customfield-date-detail.html',
-						'<dl ng-if="!$root.isEmpty(imModel.customfields[field.databaseName])">\
+						'<dl ng-if="!$root.isEmpty(goModel[field.databaseName])">\
 							<dt>{{field.name}}</dt>\
-							<dd>{{imModel.customfields[field.databaseName] | date:\'longDate\'}}</dd>\
+							<dd>{{goModel[field.databaseName] | date:\'longDate\'}}</dd>\
 						</dl>');
 						
 				$templateCache.put('customfield-datetime.html',
@@ -37,32 +37,23 @@ angular.module('GO.core')
 						
 						
 				$templateCache.put('customfield-number-detail.html',
-						'<dl ng-if="!$root.isEmpty(imModel.customfields[field.databaseName])">\
+						'<dl ng-if="!$root.isEmpty(goModel[field.databaseName])">\
 							<dt>{{field.name}}</dt>\
-							<dd>{{imModel.customfields[field.databaseName] | number}}</dd>\
+							<dd>{{goModel[field.databaseName] | number}}</dd>\
 						</dl>');
 
 				return {
 					restrict: 'E',
 					scope: {
-						imModel: '=',
-						imServerModel: '@'
+						goModel: '=ngModel',
+						serverModel: '@'
 					},
 					controller: ['$scope','$element','$attrs','$transclude', 'CustomFields', function($scope, $element, $attrs, $transclude, CustomFields) {
 
-						$scope.customFieldSetStore = CustomFields.getFieldSetStore($attrs.imServerModel);
-						
-						$scope.datePickerOpened = {};
-						
-						$scope.openDatePicker = function(id, $event) {
-							$event.preventDefault();
-							$event.stopPropagation();
-
-							$scope.datePickerOpened[id] = true;
-						};
+						$scope.customFieldSetStore = CustomFields.getFieldSetStore($attrs.serverModel);						
 
 					}],
-					template: '<div class="go-card"  ng-repeat="fieldSet in customFieldSetStore.items">\
+						template: '<ul class="properties" ng-repeat="fieldSet in customFieldSetStore.items">\
 							<h1>{{fieldSet.name}}</h1>\
 								<ng-include ng-repeat="field in fieldSet.fields" src="\'customfield-\'+field.type+\'-detail.html\'"></ng-include>\
 							</div>'
